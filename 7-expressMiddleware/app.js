210efd12512776800031c065823f69d953017dac
Mufil Rahman A <ramanizum@gmail.com>
2023-09-13 22:52:38 +0530

    middle ware added to express project

---
+++ 7-expressMiddleware/app.js
@@ -0,0 +1,14 @@
+const http = require(`http`);
+const express = require(`express`);
+
+const app = express();
+app.use((request,response,next)=>{
+console.log('in the middle ware');
+next();
+})
+app.use((request,response,next)=>{
+console.log('in the second middle ware');
+})
+
+const server = http.createServer(app);
+server.listen(3000);
