36f27353b067dc3e1679b18323d13481224c5da6
Mufil Rahman A <ramanizum@gmail.com>
2023-09-14 15:22:32 +0530

    handling different routes

--- 8-usingmodule/app.js
+++ 8-usingmodule/app.js
@@ -4,20 +4,24 @@ const express = require('express');
 //CREATE AN INSTANCE OF THE EXPRESS APPLICATION
 const app = express();
 
-//DEFINE A ROUTE FOR THE /product PATH
-app.use('/product',(request,response,next)=>{
-    response.send('<h1>This is cart page </h1>')
-})
 //DEFINE A ROUTE FOR THE /add-product PATH
-app.use('/add-product',(request, response, next) => {
-    // Send a response form for all incoming requests
+app.post('/add-product',(request, response, next) => {
+    // Send a response just for /add-product incoming requests
     response.send('<form action="/product" method="post"> <input type="text" name="product"><button>Add product</submit></form> ');
 })
+
+//DEFINE A ROUTE FOR THE /product PATH
+app.post('/product',(request,response,next)=>{
+    // Send a response just for /product incoming requests
+    response.send('<h1>This is cart page </h1>')
+})
 //DEFINE A DEFAULT ROUTE FOR THE ROOT PATH '/'
-app.use('/',(request,response,next)=>{
+app.get('/',(request,response,next)=>{
+    // Send a response form for all incoming requests
     response.send('<h1> This is Home page</h1> <form action ="/add-product" method="post"> <button>Add your product</button></form>')
 })
-//STRAT THE SERVER AND LISTEN ON PORT 3000
+//START THE SERVER AND LISTEN ON PORT 3000
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
