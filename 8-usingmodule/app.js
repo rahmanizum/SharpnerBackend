66448e4f7a3d5087a67af3fb55743203da68fad9
Mufil Rahman A <ramanizum@gmail.com>
2023-09-14 15:32:40 +0530

    added new middle ware for parsing body

--- 8-usingmodule/app.js
+++ 8-usingmodule/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 //CREATE AN INSTANCE OF THE EXPRESS APPLICATION
 const app = express();
 
+//CREATE A MIDDLE WARE DOING PARSING 
+app.use(express.urlencoded({extended:false}));
+
 //DEFINE A ROUTE FOR THE /add-product PATH
 app.post('/add-product',(request, response, next) => {
     // Send a response just for /add-product incoming requests
@@ -12,11 +15,13 @@ app.post('/add-product',(request, response, next) => {
 
 //DEFINE A ROUTE FOR THE /product PATH
 app.post('/product',(request,response,next)=>{
+    //get body to pass to browser
+    console.log(request.body);
     // Send a response just for /product incoming requests
     response.send('<h1>This is cart page </h1>')
 })
 //DEFINE A DEFAULT ROUTE FOR THE ROOT PATH '/'
-app.get('/',(request,response,next)=>{
+app.use('/',(request,response,next)=>{
     // Send a response form for all incoming requests
     response.send('<h1> This is Home page</h1> <form action ="/add-product" method="post"> <button>Add your product</button></form>')
 })
