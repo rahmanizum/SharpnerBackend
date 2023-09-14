2172fb42fad17707fe7a798da960fc8e60a5f52c
Mufil Rahman A <ramanizum@gmail.com>
2023-09-15 00:47:28 +0530

    added data to a txt file

--- 9-addingHtml/app.js
+++ 9-addingHtml/app.js
@@ -8,17 +8,21 @@ const app = express();
 //DEFINE A MIDDLE WARE TO PARSE BODY 
 app.use(express.urlencoded({ extended: true }));
 
-//DEFINE A ROUTE FOR THE /product PATH
-app.use('/product',(request,response,next)=>{
-    const body = request.body;
-    console.log(body);
-    response.send(`<h1>This is cart page </h1> <h2>product name: ${body.product[0]} <br> size: ${body.product[1]}`)
-})
 //DEFINE A ROUTE FOR THE /add-product PATH
 app.use('/add-product',(request, response, next) => {
     // Send a response form for all incoming requests
     response.send('<form action="/product" method="post"><input type="text" name="product" placeholder="Product Name"><input type="number" name="product" placeholder="Product Size"><button>Add product</submit></form> ');
 })
+
+//DEFINE A ROUTE FOR THE /product PATH
+app.use('/product',(request,response,next)=>{
+    const body = request.body;
+    fs.appendFile('productData.txt',`${body.product}\n`,(err)=>{
+        if(err) console.log(err);
+        response.send(`<h1>This is cart page </h1> <h2>product name: ${body.product[0]} <br> size: ${body.product[1]}`)
+    })
+})
+
 //DEFINE A DEFAULT ROUTE FOR THE ROOT PATH '/'
 app.use('/',(request,response,next)=>{
     response.send('<h1> This is Home page</h1> <form action ="/add-product" method="post"> <button>Add your product</button></form>')
