f241bbdabc634f7882ce52653b0171b1931ff1b2
Mufil Rahman A <ramanizum@gmail.com>
2023-09-15 01:05:46 +0530

    set up page not found

--- 9-addingHtml/app.js
+++ 9-addingHtml/app.js
@@ -17,6 +17,13 @@ app.use(AdminRouter);
 //CREATE A MIDDLE WARE FOR SHOP 
 app.use(shopRouter);
 
+//CREATE A MIDDLE WATE FOR STATUS CODE 404
+
+app.use('/',(request,response,next)=>{
+    response.status(404).send(`<h1>404 - Page Not Found</h1>
+    <h4>The page you are looking for might have been removed or is temporarily unavailable.</h4>`);
+})
+
 //START THE SERVER AND LISTEN ON PORT 3000
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
